1977cf2c20f24754af1ef0081c8d5a074dbd72e3
Joshua Diaz <jad24096@gmail.com>
2022-05-22 22:09:05 -0400

    added package json and also started workign on thought controller.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,6 +1,53 @@
-//get all thoughts
-//get a single thought
-// create a new thought
+const { Thought } = require("../models")
+
+
+module.exports = {
+    //get all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thought) => res.json(courses))
+            .catch((err) => res.status(500).json(err));
+    },
+    //get a single thought
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughid })
+            .select('-_v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    //create a new thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    //delete a thought
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((thought) => 
+            !thought
+            ? res.status(404).json({ message: 'No thought with that ID' })
+            : User.deleteMany({ _id: { $in: thought.user } })
+            )
+            .then(() => res.json({ message: 'User and Thought deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    },
+    //update a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+
+        )
+    }
+}
+
+
 //update a thought
 //delete a thought
 //post a reaction
--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -2,6 +2,5 @@ const addDate = date => {
     let dateString = date.toString();
 
     const lastCharacter = dateString.characterAt(dateString.length -1);
-
     
 }
